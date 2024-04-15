01689f49a005586d75a92307fb4cc37512724f08
andrewWong21 <awong541@gmail.com>
2024-04-15 16:13:42 -0400

    13.3.1 Exercises

--- r4ds/13-numbers_13-3-1.R
+++ r4ds/13-numbers_13-3-1.R
@@ -17,7 +17,70 @@ parse_double(x)
 y <- c("$1,234", "USD 5,313", "59%")
 parse_number(y)
 
+# count() is useful for quick exploration and checks during analysis
+flights |> count(dest)
+
+# sort by most common values with sort = TRUE
+flights |> count(dest, sort = TRUE)
+
+# same result can be achieved with group_by(), summarize(), and n(),
+# but other summaries can be computed at the same time as well
+flights |> 
+  group_by(dest) |> 
+  summarize(
+    n = n(),
+    delay = mean(arr_delay, na.rm = TRUE)
+  )
+
+# n() does not take any arguments,
+# accesses information about current group only,
+# so it does not work outside of dplyr verbs
+n()
+
+# n_distinct() counts number of distinct values of one or more variables
+flights |> 
+  group_by(dest) |> 
+  summarize(carriers = n_distinct(carrier)) |> 
+  arrange(desc(carriers))
+
+# calculating weighted count of miles for each plane
+flights |>
+  group_by(tailnum) |> 
+  summarize(miles = sum(distance))
+
+# specify wt in count() for calculating weighted counts
+flights |> count(tailnum, wt = distance)
+
+# count missing values by combining sum() and is.na()
+flights |> 
+  group_by(dest) |> 
+  summarize(n_cancelled = sum(is.na(dep_time)))
+
 # -------------------------------------------------------------------------
 
 # 1. How can you use count() to count the number rows with
 # a missing value for a given variable?
+
+df <- tibble(
+  x = c(1, 2, 3, 4, 5, 6, 7, 8),
+  y = c(3, NA, 7, NA, NA, 5, 4, 2)
+)
+
+df |> count(is.na(y))
+
+
+# 2. Expand the following calls to count()
+# to instead use group_by(), summarize(), and arrange():
+
+flights |> count(dest, sort = TRUE)
+
+flights |> 
+  group_by(dest) |> 
+  summarize(n = n()) |> 
+  arrange(desc(n))
+
+flights |> count(tailnum, wt = distance)
+
+flights |> 
+  group_by(tailnum) |> 
+  summarize(n = sum(distance))
