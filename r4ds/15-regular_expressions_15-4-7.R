113f0cb864df2d4948c77ad3b815447e9e76b2a0
andrewWong21 <awong541@gmail.com>
2024-05-02 23:53:25 -0400

    15.4.7 Exercises

--- r4ds/15-regular_expressions_15-4-7.R
+++ r4ds/15-regular_expressions_15-4-7.R
@@ -113,39 +113,172 @@ str_view(abc_strs, "(ab)+")
 # equivalent to "(^a)|(b$)"
 str_view(abc_strs, "^a|b$")
 
+# parentheses also create capturing groups that can be back-referenced
+# \1 refers to first captured group, \2 is second, etc.
+
+# find all fruits with repeated pairs of letters
+str_view(fruit, "(..)\\1")
+
+# find all words that start and end with the same pair of letters
+str_view(words, "^(..).*\\1$")
+
+# using back-references in str_replace() to swap 2nd and 3rd words in sentences
+sentences |> 
+  str_replace("(\\w+) (\\w+) (\\w+)", "\\1 \\3 \\2") |> 
+  str_view()
+
+# str_match() returns matches in the form of a matrix
+sentences |> 
+  str_match("the (\\w+) (\\w+)") |> 
+  head()
+
+# converting to tibble and naming columns effectively recreates
+# the function of separate_wider_regex()
+sentences |> 
+  str_match("the (\\w+) (\\w+)") |> 
+  as_tibble(.name_repair = "minimal") |> 
+  set_names("match", "word1", "word2")
+
+# use parentheses without creating capturing group by using (?:)
+g <- c("a gray cat", "a grey dog")
+str_match(g, "gr(e|a)y")
+str_match(g, "gr(?:e|a)y")
+
 # -------------------------------------------------------------------------
 
 # 1. How would you match the following literal strings?
 
 # ".\
+qdb <- r"{aaa".\bbb}"
+str_view(qdb, "\"\\.\\\\")
+str_view(qdb, r"{".\\}")
 
 # $^$
+dcd <- "aaa$^$bbb"
+str_view(dcd, "\\$\\^\\$")
+str_view(dcd, r"{\$\^\$}")
 
 
 # 2. Explain why each of these patterns don't match a literal backslash:
 
+backslash <- "\\"
+str_view(backslash, "\\\\")
+
 # "\"
+# invalid string, backslash escapes ending quote
 
 # "\\"
+# invalid regex, escaped backslash is read as metacharacter in regex
 
 # "\\\"
+# invalid string, third backslash escapes ending quote
 
 
 # 3. Given the corpus of common words in stringr::words,
 # create regular expressions to find words that match the following patterns:
 
 # start with y
+str_view(words, "^y")
 
 # don't start with y
+str_view(words, "^[^y]")
 
 # end with x
+str_view(words, "x$")
 
 # are exactly 3 letters long (without using str_length)
+str_view(words, "^\\w{3}$")
 
 # have seven letters or more
+str_view(words, "^\\w{7,}$")
 
 # contain a vowel-consonant pair
+str_view(words, "[aeiou][^aeiou]")
 
 # contain at least two vowel-consonant pairs in a row
+str_view(words, "([aeiou][^aeiou])\\1")
 
 # only consist of repeated vowel-consonant pairs
+str_view(words, "^([aeiou][^aeiou])\\1*$")
+
+
+# 4. Create 11 regexps that match the British or American spellings for:
+
+# airplane/aeroplane
+str_view(c("airplane", "aeroplane"), "a(ir|ero)plane")
+
+# aluminum/aluminium
+str_view(c("aluminum", "aluminium"), "alumini?um")
+
+# analog/analogue
+str_view(c("analog", "analogue"), "analog(ue)?")
+
+# ass/arse
+str_view(c("ass", "arse"), "a(ss|rse)")
+
+# center/centre
+str_view(c("center", "centre"), "cent(er|re)")
+
+# defense/defence
+str_view(c("defense", "defence"), "defen(s|c)e")
+
+# donut/doughnut
+str_view(c("donut", "doughnut"), "do(ugh)?nut")
+
+# gray/grey
+str_view(c("gray", "grey"), "gr(a|e)y")
+
+# modeling/modelling
+str_view(c("modeling", "modelling"), "defen(s|c)e")
+
+# skeptic/sceptic
+str_view(c("skeptic", "sceptic"), "s(k|c)eptic")
+
+#summarize/summarise
+str_view(c("summarize", "summarise"), "summari(z|s)e")
+
+
+# 5. Switch the first and last letters in words.
+# Which of those strings are still words?
+
+words[words %in% str_replace(words, "^(.)(.*)(.)$", "\\3\\2\\1")]
+
+
+# 6. Describe what the following regular expressions match:
+
+# <^.*$>
+# matches any character 0 or more times (any string)
+str_view(c("", "a", "aa", "ab", "aaa"), r"{^.*$}")
+
+# "\\{.+\\}"
+# matches curly brackets surrounding at least one or more characters
+str_view(c("{}", "{abc}", "{ab}", "{def}{gh}"), "\\{.+\\}")
+
+# <\d{4}-\d{2}-\d{2}>
+# matches group of 4 digits, 2 digits, 2 digits separated by dashes
+str_view(
+  c("1234-56-78", "123-45-67", "111111-11-11-111-11-1"), 
+  r"{\d{4}-\d{2}-\d{2}}"
+)
+
+# "\\\\{4}"
+# matches four literal backslashes
+str_view(c(r"{\\\\}", r"{\\\\\\}", "\\\\\\\\\\"), "\\\\{4}")
+
+# <\..\..\..>
+# matches three dots each followed by one character
+str_view(c(".a.b.c", "d.e.f.gh.i.j.k" ), r"{\..\..\..}")
+
+# <(.)\1\1>
+# matches any character that is repeated three times
+str_view(c("aaa", "bb", "ccccc", "dddddd", "abc"), r"{(.)\1\1}")
+
+# "(..)\\1"
+# matches a group of any two characters repeated twice
+str_view(c("aaaa", "abab", "abcd", "1212121212"), "(..)\\1")
+
+
+# 7. Solve the beginner regexp crosswords at
+# https://regexcrosswords.com/challenges/beginner
+
+# HELP, BOBE, OOOO, **//, 1984
