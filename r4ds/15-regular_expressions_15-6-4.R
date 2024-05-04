b0562dc49a478775283da503b2ae8a29b95992c4
andrewWong21 <awong541@gmail.com>
2024-05-04 19:57:05 -0400

    15.7.2 Base R regular expressions

--- r4ds/15-regular_expressions_15-6-4.R
+++ r4ds/15-regular_expressions_15-6-4.R
@@ -87,4 +87,98 @@ words[
   str_detect(words, "u")
 ]
 
+# finding all sentences that contain the colors red, green, or blue
+str_view(sentences, "\\b(red|green|blue)\\b")
+
+# storing colors in vector that can be expanded as needed
+# and creating full pattern using str_c() and str_flatten()
+rgb <- c("red", "green", "blue")
+str_c("\\b(", str_flatten(rgb, "|"), ")\\b")
+
+# building a list of colors from non-numbered built-in colors in R
+str_view(colors())
+cols <- colors()
+cols <- cols[!str_detect(cols, "\\d")]
+str_view(cols)
+
+color_pattern <- str_c("\\b(", str_flatten(cols, "|"), ")\\b")
+str_view(sentences, color_pattern)
+
+# tidyverse usages for regex:
+# matches(pattern) selects all variables with pattern-matching names
+# can be used with functions like select(), rename_with(), across()
+# 
+# pivot_longer() has a name_pattern argument that takes regex vectors
+# 
+# delim argument in separate_longer_delim() and separate_wider_delim()
+# can be used with regex to match patterns
+
+# base R uses for regex:
+# apropos(pattern) searches global environment for objects matching pattern
+apropos("replace")
+
+# base R uses slightly different pattern language compared to stringr
+# difference only becomes apparent with more advanced features
+
 # -------------------------------------------------------------------------
+
+# 1. For each of the following challenges, try solving it using both
+# a single regular expression and a combination of str_detect() calls.
+
+# Find all words that start or end with x.
+str_view(words, "^x|x$")
+words[str_detect(words, "^x") | str_detect(words, "x$")]
+
+# Find all words that start with a vowel and end with a consonant.
+str_view(words, "^[aeiou].+[^aeiou]$")
+words[str_detect(words, "^[aeiou]") & !str_detect(words, "[aeiou]$")]
+
+# Are there any words that contain at least one of each different vowel?
+words[
+  str_detect(words, "a") &
+  str_detect(words, "e") &
+  str_detect(words, "i") &
+  str_detect(words, "o") &
+  str_detect(words, "u")
+]
+
+
+# 2. Construct patterns to find evidence for and against the rule
+# "i before e except after c".
+
+# evidence for rule:
+# words with c-e-i consecutively
+str_view(words, "cei")
+# words with i before e when not after c
+str_view(words, "[^c]ie")
+
+# evidence against:
+# words with c-i-e consecutively
+str_view(words, "cie")
+# words with i after e  when not after c
+str_view(words, "[^c]ei")
+
+
+# 3. colors() contains a number of modifiers like "lightgray" and "darkblue".
+# How could you automatically identify these modifiers?
+
+modified <- c("pale", "medium", "light", "dark")
+colors()[str_detect(colors(), str_flatten(modified, "|"))]
+
+
+# 4. Create a regular expression that finds any base R dataset.
+sets <- data(package = "datasets")$results[, "Item"]
+# capture everything before " ("
+stripped <- str_match(sets, "(.*) \\(")[,2]
+stripped <- str_replace(stripped, "\\.", "\\\\.")
+
+# 
+dataset_regex <- str_flatten(
+  c(
+    stripped[!is.na(stripped)], 
+    sets[!str_detect(sets, "\\(")]
+  ),
+  "|"
+)
+
+str_view("beaver2 freeny.y freeny state.x77 state.abb", dataset_regex)
