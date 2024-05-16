2966bf5579b8091415ef4aa1ae1a4d30b8626747
andrewWong21 <awong541@gmail.com>
2024-05-15 20:51:56 -0400

    18.3.4 Exercises

--- r4ds/18-missing_values-18-3-4.R
+++ r4ds/18-missing_values-18-3-4.R
@@ -25,12 +25,13 @@ coalesce(x, 0)
 y <- c(1, 4, 5, 7, -99)
 na_if(y, -99)
 
+# special missing value NaN that usually behaves like NA
 z <- c(NA, NaN)
 z * 10
 z == 1
 is.na(z)
 
-# use is.nan() if distinguishing NA from NaN is important
+# use is.nan(x) if distinguishing NA from NaN is important
 is.nan(z)
 
 # NaN occurs when performing mathematical operation with indeterminate result
@@ -51,18 +52,36 @@ stocks <- tibble(
 # price in 4th quarter of 2020 is explicitly missing, value is NA
 # price in 1st quarter of 2021 is implicitly missing, does not appear in data
 
-# pivoting makes implicit missing values explicit
+# may want to convert implicit to explicit to have values to work with
+# or convert explicit to implicit by changing structure of data
+
+# pivoting can be used to convert implicit to explicit (pivot wider)
+# and explicit to implicit (pivot longer)
 # every combination of rows and new columns must have some value
 
-stocks |> 
+stocks_wider <- stocks |> 
   pivot_wider(
     names_from = qtr,
     values_from = price
   )
 
 # making data longer preserves explicit missing values
+stocks_wider |> 
+  pivot_longer(
+    cols = c(`1`, `2`, `3`, `4`),
+    names_to = "qtr",
+    values_to = "price"
+  )
+
 # structurally missing values due to untidy data can be dropped
-# by specifying values_drop_na = TRUE
+# by specifying values_drop_na = TRUE within pivot_longer()
+stocks_wider |> 
+  pivot_longer(
+    cols = c(`1`, `2`, `3`, `4`),
+    names_to = "qtr",
+    values_to = "price",
+    values_drop_na = TRUE
+  )
 
 # tidyr::complete() allows generating explicit missing values by defining
 # a combination of rows that should exist from the values of certain variables
@@ -98,7 +117,85 @@ flights |>
   distinct(tailnum) |> 
   anti_join(planes)
 
+# when working with factor groups, some groups may be empty,
+# meaning that the group does not contain any observations
+health <- tibble(
+  name = c("Ikaia", "Oletta", "Leriah", "Dashay", "Tresaun"),
+  smoker = factor(c("no", "no", "no", "no", "no"), levels = c("yes", "no")),
+  age = c(34, 88, 75, 47, 56)
+)
+
+# only non-smokers counted in dataset
+health |> count(smoker)
+
+# specify including zero-observation groups in count() with .drop = FALSE
+health |> count(smoker, .drop = FALSE)
+
+# ggplot will also drop empty groups by default,
+# specify drop = FALSE for the corresponding axis
+ggplot(health, aes(x = smoker)) + 
+  geom_bar()
+
+ggplot(health, aes(x = smoker)) + 
+  geom_bar() + 
+  scale_x_discrete(drop = FALSE)
+
+# same problem can occur with dplyr::group_by(), use .drop = FALSE
+health |> 
+  group_by(smoker, .drop = FALSE) |> 
+  summarize(
+    n = n(),
+    mean_age = mean(age),
+    min_age = min(age),
+    max_age = max(age),
+    sd_age = sd(age)
+  )
+
+# summarizing an empty group applies the summary functions to 0-length vectors
+# empty vectors have length 0, missing values have length 1
+x1 <- c(NA, NA)
+length(x1)
+
+x2 <- numeric()
+length(x2)
+
+# mean(x) when x is empty returns sum(x)/length(x) = 0/0, which returns NaN
+# min(x) and max(x) when x is empty returns -Inf and Inf, respectively
+# min(c(x, y)) is always equal to min(min(x), min(y))
+
+# performing the summary, then using complete() results in all summaries
+# being NA for the empty group, even though the count should be 0
+health |> 
+  group_by(smoker) |> 
+  summarize(
+    n = n(),
+    mean_age = mean(age),
+    min_age = min(age),
+    max_age = max(age),
+    sd_age = sd(age)
+  ) |> 
+  complete(smoker)
+
 # -------------------------------------------------------------------------
 
-# 1. Can you find any relationship between the carrier and the rows that appear
-# to be missing from planes?
+# 1. Can you find any relationship between the carrier
+# and the rows that appear to be missing from planes?
+
+# flights and airlines datasets have a variable called "carrier"
+# planes does not have a "carrier" column, but can be joined with
+# flights on the column "tailnum"
+colnames(flights)[colnames(flights) %in% colnames(planes)]
+
+carriers <- flights |>
+  anti_join(planes, by = "tailnum") |> 
+  distinct(carrier) |> 
+  pull()
+
+airlines |> 
+  filter(carrier %in% carriers)
+
+# planes dataset is missing tailnums for 10 carriers
+flights |>
+  anti_join(planes, by = "tailnum") |> 
+  distinct(carrier) |> 
+  left_join(airlines)
