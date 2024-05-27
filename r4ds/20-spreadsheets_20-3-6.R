a1d35454400640d330f1d50cb020063ea9452e6f
andrewWong21 <awong541@gmail.com>
2024-05-26 23:40:09 -0400

    20.3.6 Exercises

--- r4ds/20-spreadsheets_20-3-6.R
+++ r4ds/20-spreadsheets_20-3-6.R
@@ -1,6 +1,7 @@
 {
   library("tidyverse")
   library("googlesheets4")
+  library("readxl")
 }
 
 # Google Sheets is a free web-based spreadsheet program
@@ -58,5 +59,33 @@ bake_sale
 write_sheet(bake_sale, ss = "bake-sale-gs")
 write_sheet(bake_sale, ss = "bake-sale-gs", sheet = "Sales")
 
+# reading sheets can be done with gs4_deauth() to avoid authenticating, but
+# writing to a sheet requires authentication with Google account
+
+# specify Google account with gs4_auth(email = "mine@example.com")
+
 # -------------------------------------------------------------------------
 
+# 1. Read the students dataset from Excel and also Google Sheets with no 
+# additional arguments supplied to the read_excel() and read_sheet() functions.
+# Are the resulting dataframes exactly the same? If not, how are they different?
+
+read_excel("data/students.xlsx")
+read_sheet(students_sheet_id)
+
+# reading from .xlsx file results in AGE column having type <chr>
+# reading from Google Sheet results in AGE column having type <list>
+# with four lists containing a double, one list containing a character variable,
+# and one NULL object
+
+# 2. Read the Google Sheet titled survey from https://pos.it/r4ds-survey
+# with survey_id as a character variable and n as a numeric variable.
+survey_sheet_id <- "1yc5gL-a2OOBr8M7B3IsDNX5uR17vBHOyWZq6xSTG2G8"
+read_sheet(survey_sheet_id, col_types = "cd") # character, double
+
+
+# 3. Read the Google Sheet titled roster from https://pos.it/r4ds-roster
+# with all missing values filled in.
+roster_sheet_id <- "1LgZ0Bkg9d_NK8uTdP2uHXm07kAlwx8-Ictf8NocebIE"
+read_sheet(roster_sheet_id) |> 
+  fill(group, subgroup)
