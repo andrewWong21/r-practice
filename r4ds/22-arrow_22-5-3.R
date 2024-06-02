db87d27ad9de9ffce910af79e713ab7fe478c94d
andrewWong21 <awong541@gmail.com>
2024-06-01 23:55:00 -0400

    22.4.3 Rewriting the Seattle library data

--- r4ds/22-arrow_22-5-3.R
+++ r4ds/22-arrow_22-5-3.R
@@ -38,3 +38,41 @@ seattle_csv
 # 41 million rows, 12 columns, and some values in each column
 # 3 int64 columns, 9 string columns
 seattle_csv |> glimpse()
+
+# use collect() to perform computation with arrow
+# takes some time to run, can be made faster with different data format
+seattle_csv |> 
+  group_by(CheckoutYear) |> 
+  summarize(Checkouts = sum(Checkouts)) |> 
+  arrange(CheckoutYear) |> 
+  collect()
+
+# parquet is a custom binary format for handling big rectangular data
+# has more efficient encodings compared to csv files, making them smaller
+# can store data type along with data (csv files must guess string or date)
+# column-oriented like data frames in R, improving data analysis performance
+# files are "chunked" - can work on different parts at the same time
+
+# disadvantage - no longer human-readable
+
+# best partitioning depends on data, access patterns, and reading systems
+# do experimenting to find which process works best
+# arrow recommends file sizes >20 MB and <2 GB, avoid producing 10,000+ files
+# additionally, partition by variables to be filtered by
+# to avoid reading irrelevant files
+
+# partitioning large dataset across many files
+# partition seattle_csv by CheckoutYear, producing 18 chunks
+# define partition with dplyr::group_by() and saving partitions
+# to directory with arrow::write_dataset(path, format)
+
+# pq_path <- "data/seattle-library-checkouts"
+# seattle_csv |> 
+#   group_by(CheckoutYear) |> 
+#   write_dataset(pq_path, format = "parquet")
+
+# rewrote 9GB csv file into 18 parquet files of 100-300 MB each, ~4GB total
+tibble(
+  files = list.files(pq_path, recursive = TRUE),
+  size_MB = file.size(file.path(pq_path, files)) / 1024^2
+)
