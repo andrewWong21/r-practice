b404a6b8b444a170daee47a407e0bb819388700e
andrewWong21 <awong541@gmail.com>
2024-07-06 23:53:58 -0400

    28.3 Visual editor

---
+++ r4ds/28-quarto_28-3-1.R
@@ -0,0 +1,22 @@
+# visual editor in RStudio is WYSIWYM interface for writing .qmd files
+# (what you see is what you mean)
+
+# Quarto uses Pandoc markdown for text
+# provides support for tables, citations, cross-references, footnotes,
+# divs/spans, definition lists, attributes, raw HTML/TeX, etc.
+
+# start with visual editor if more experienced with Google Docs or MS Word
+# over writing computational documents like .qmd files
+
+# use buttons on menu bar to insert images, tables, etc.
+# or use keyboard shortcut Ctrl + /
+
+# italic with one pair of asterisks
+# bold with two pairs of asterisks
+# underline with 
+# strikethrough with two pairs of tildes
+# superscript by surrounding with caret ^
+# subscript by surrounding with single underscore
+
+# -------------------------------------------------------------------------
+
