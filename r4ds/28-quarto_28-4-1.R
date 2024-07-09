aea92ca0b9da72ab790481ff920c27b95aa090f2
andrewWong21 <awong541@gmail.com>
2024-07-08 23:22:47 -0400

    28.4.1 Exercises

---
+++ r4ds/28-quarto_28-4-1.R
@@ -0,0 +1,40 @@
+# source editor will feel more familiar to those experienced with
+# writing R scripts or R Markdown documents
+
+# Help > Markdown Quick Reference provides reference sheet
+
+# text formatting (emphasis)
+# italicize with one pair of asterisks/underscores, bold with two pairs
+
+# headings are prefaced with 1 to 3 asterisks at the beginning of a line
+
+# create unordered list with - before each item
+# can indent before - to create subitems within lists
+
+# for ordered lists, preface with 1. 2. 3. and so on
+
+# end line with two or more spaces to create manual line break
+
+# create link with plain http address
+# or add link to phrase with [phrase](link)
+
+# create image with url or file path, with optional alt text
+# ![alt text](path)
+
+# create block quote with > before text
+
+# table
+# First Header  | Second Header
+# ------------- | -------------
+# Content Cell  | Content Cell
+# Content Cell  | Content Cell
+
+# create footnote with [^id]
+# [^id]: note_text
+
+# -------------------------------------------------------------------------
+
+# Copy and paste the contents of diamond-sizes.qmd from 
+# https://github.com/hadley/r4ds/tree/main/quarto into a
+# local R Quarto document. Check that you can run it, then add text
+# after the frequency polygon that describes its most striking features.
