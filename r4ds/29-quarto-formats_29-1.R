ef2b5a5ec7f1641d50fde188dba25797d2e8e05f
andrewWong21 <awong541@gmail.com>
2024-07-25 23:54:02 -0400

    29.4 Presentations

--- r4ds/29-quarto-formats_29-1.R
+++ r4ds/29-quarto-formats_29-1.R
@@ -32,3 +32,26 @@ quarto::quarto_render(
 
 # use output_format = "all" to render formats specified in YAML header
 quarto::quarto_render("diamond-sizes.qmd", output_format = "all")
+
+# other formats supported by Quarto:
+# pdf, for Portable Document Format documents, requires installing LaTeX
+# docx, for Microsoft Word documents
+# odt, for OpenDocument Text documents
+# rtf, for Rich Text Format documents
+# gfm, for GitHub Flavored Markdown documents (.md)
+# ipynb, for Jupyter Notebooks
+
+# when outputting to html, option override with code: true 
+# to hide code chunks by default but make them visible with a click
+# format:
+#   html:
+#     code: true
+
+# Quarto can be used to create presentations
+# content of document is divided into slides separated by headers
+# new slides begin at second level header ##
+# first level header # indicate sections with centered section title slide
+
+# Quarto supports presentation formats including
+# revealjs (HTML), pptx (PowerPoint), beamer (PDF with LaTeX Beamer)
+# read more at https://quarto.org/docs/presentations/
