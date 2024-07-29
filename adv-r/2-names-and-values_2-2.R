0480c0bc3687979216b170c60eedd1df2be088e2
andrewWong21 <awong541@gmail.com>
2024-07-28 23:57:50 -0400

    2.1 Introduction

---
+++ adv-r/2-names-and-values_2-2.R
@@ -0,0 +1,16 @@
+# knowing distinction between names and objects is important
+# improves predictions of code performance and memory usage
+# avoiding accidental copies of code leads to faster code
+# improves understanding of functional programming tools in R
+
+# use lobstr package to dig into internal R object representations
+library(lobstr)
+
+# distinction between names and values
+
+# situations where R makes copies
+
+# memory occupied by objects
+
+# -------------------------------------------------------------------------
+
