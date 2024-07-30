7625817dca9ffd0cbbf5bfee32add6fadb030ed2
andrewWong21 <awong541@gmail.com>
2024-07-29 23:57:33 -0400

    2.2 Binding basics

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # r-practice
-Hands-on learning with R with [R for Data Science](https://r4ds.hadley.nz/data-visualize)
+Hands-on learning with R with books by Hadley Wickham
+- [R for Data Science](https://r4ds.hadley.nz/data-visualize)
+- [Advanced R](https://adv-r.hadley.nz/index.html)
--- adv-r/2-names-and-values_2-2.R
+++ adv-r/2-names-and-values_2-2.R
@@ -1,12 +1,29 @@
 # knowing distinction between names and objects is important
-# improves predictions of code performance and memory usage
-# avoiding accidental copies of code leads to faster code
-# improves understanding of functional programming tools in R
+# improve accuracy of predictions of code performance and memory usage
+# write faster code by avoiding accidental copies of code
+# improve understanding of functional programming tools in R
 
 # use lobstr package to dig into internal R object representations
 library(lobstr)
 
 # distinction between names and values
+
+# the following statement does two things:
+# creates a object - vector of values 1, 2, 3
+# binds object to name, "x"
+x <- c(1, 2, 3)
+
+# crucial distinction: object is value, name has value
+# i.e. name is reference to value, object does not have name
+
+# the following statement creates another binding for existing object in memory
+# does not create copy of value
+y <- x
+
+# access object identifier with lobstr::obj_addr()
+# identifiers change every time R is restarted
+obj_addr(x)
+obj_addr(y)
 
 # situations where R makes copies
 
@@ -14,3 +31,17 @@ library(lobstr)
 
 # -------------------------------------------------------------------------
 
+# 1. Given the following data frame, create a new column "3" that contains
+# the sum of columns "1" and "2" while only using $.
+# What makes "1", "2", "3" challenging as variable names?
+df <- data.frame(runif(3), runif(3))
+names(df) <- c(1, 2)
+
+# 2. In the following code, how much memory does "y" occupy?
+x <- runif(1e6)
+y <- list(x, x, x)
+
+# 3. On which line does "a" get copied in the following example?
+a <- c(1, 5, 3, 2)
+b <- a
+b[[1]] <- 10
