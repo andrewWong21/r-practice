5d3c702f2d352c44bbc1cbc63e45ef03514a6b4c
andrewWong21 <awong541@gmail.com>
2024-08-02 23:58:52 -0400

    2.3.4 Data frames

--- adv-r/2-names-and-values_2-3.R
+++ adv-r/2-names-and-values_2-3.R
@@ -24,3 +24,51 @@ y[[3]] <- 5L
 
 # untracemem turns tracing off
 untracemem(x)
+
+# copying rules also apply to functions
+f <- function(a){
+  a
+}
+
+x <- c(1, 2, 3)
+cat(tracemem(x), "\n")
+
+z <- f(x)
+
+untracemem(x)
+
+# since function f() does not modify its argument a, no copy is made of a
+# x and z point to the same object after f() finishes running
+# while f() is running, a and x point to the same object
+
+# lists store references to values - each element is a pointer to a value
+list1 <- list(1, 2, 3)
+
+# lists and vectors both have copy-on-modify behavior
+# R makes a shallow copy when modifying lists
+# bindings are copied, but values are not copied
+# deep copies also copy contents of references into separate memory addresses
+list2 <- list1
+list2[[3]] <- 4
+
+# use lobstr::ref() to see values shared across lists
+# ref() prints memory address and local id of objects for cross-referencing
+
+# elements 2 and 3 of list1 and list2 point to the same object in memory
+lobstr::ref(list1, list2)
+
+# data frames are lists of vectors, slightly different behavior
+# when modifying rows vs. modifying columns
+d1 <- data.frame(x = c(1, 5, 6), y = c(2, 4, 3))
+
+# modifying a column only makes a copy of that column
+# other columns are not modified
+d2 <- d1
+d2[, 2] <- d2[, 2] * 2
+lobstr::ref(d1, d2)
+
+# modifying a row makes a copy of every column
+# every column is modified
+d3 <- d1
+d3[1, ] <- d3[1, ] * 3
+lobstr::ref(d1, d3)
