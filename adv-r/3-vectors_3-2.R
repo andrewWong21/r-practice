589cc1953ff818c3075a7790d890b6f1deb1c7d3
andrewWong21 <awong541@gmail.com>
2024-08-09 23:56:43 -0400

    3.1 Introduction

---
+++ adv-r/3-vectors_3-2.R
@@ -0,0 +1,28 @@
+# 1. What are the four common types of atomic vectors?
+# What are the two rare types?
+
+# 2. What are attributes? How do you get them and set them?
+
+# 3. How is a list different from an atomic vector?
+# How is a matrix different from a data frame?
+
+# 4. Can you have a list that is a matrix?
+# Can a data frame have a column that is a matrix?
+
+# 5. How do tibbles behave differently from data frames?
+
+# -------------------------------------------------------------------------
+
+# two flavors of vectors: atomic vectors and lists
+# elements of atomic vectors must all be of the same type
+# lists may contain different types of elements
+
+# NULL is closely related to vectors, often used as generic zero-length vector
+
+# vectors can have attributes - dimension and class
+# dimensions turns vectors into matrices and arrays
+# classes are used by S3 object system
+
+# atomic vectors: logical, integer, double, character
+
+# attributes work as R's flexible metadata specification
