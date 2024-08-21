5d61d4be441c97e9bbc186854ea196102ab265d4
andrewWong21 <awong541@gmail.com>
2024-08-20 21:56:36 -0400

    3.4.5 Exercises

--- adv-r/3-vectors_3-4.R
+++ adv-r/3-vectors_3-4.R
@@ -72,5 +72,77 @@ structure(now_ct, tzone = "America/New_York")
 structure(now_ct, tzone = "Australia/Lord_Howe")
 structure(now_ct, tzone = "Europe/Paris")
 
+# durations are stored in difftimes, built on top of doubles
+# represent amount of time between pairs of dates or date-times
+# has units class to determine unit of time used
+
+one_week_1 <- as.difftime(1, units = "weeks")
+one_week_1
+typeof(one_week_1)
+attributes(one_week_1)
+unclass(one_week_1)
+
+one_week_2 <- as.difftime(7, units = "days")
+one_week_2
+typeof(one_week_2)
+attributes(one_week_2)
+unclass(one_week_2)
+
 # -------------------------------------------------------------------------
 
+# 1. What sort of object does table() return? What is its type?
+# What attributes does it have? How does the dimensionality change
+# as you tabulate more variables?
+
+table(letters)
+table(factor(letters))
+
+x <- c("a", "b", "b", "a")
+x_fact <- factor(x, levels = letters)
+x_fact
+
+table(x)
+x_tab <- table(x_fact)
+
+# table is an integer vector
+typeof(x_tab)
+
+# has attributes "dim" for length, "dimnames" for level names, and "class" table
+attributes(x_tab)
+
+y_tab = table(factor(c("a", "b", "d", "m", "b", "a"), levels = letters))
+y_tab
+attributes(y_tab)
+
+
+# 2. What happens to a factor when you modify its levels?
+
+f1 <- factor(letters)
+f1
+unclass(f1)
+
+# the underlying integer values stay the same when the levels are modified
+# z = 1, y = 2, x = 3, ..., a = 26, categories listed z-a
+# underlying elements are in order from 1 to 26
+levels(f1) <- rev(levels(f1))
+f1
+unclass(f1)
+as.integer(f1)
+
+
+# 3. What does this code do? How do f2 and f3 differ from f1?
+
+# f2 converts letters to a factor, then reverses the elements
+# f2 has reversed elements but maintains the original level order
+# a = 1, b = 2, c = 3, ..., z = 26, categories listed a-z
+# underlying elements are in reverse order from 26 to 1
+f2 <- rev(factor(letters))
+f2
+unclass(f2)
+as.integer(f2)
+
+# f3 has reversed level order on creation, element order is unchanged
+f3 <- factor(letters, levels = rev(letters))
+f3
+unclass(f3)
+as.integer(f3)
