0a010f443f59e518b57941f4d1afd11e9ad6e034
andrewWong21 <awong541@gmail.com>
2024-08-24 23:58:16 -0400

    3.6.1 Creating

--- adv-r/3-vectors_3-6.R
+++ adv-r/3-vectors_3-6.R
@@ -25,3 +25,31 @@ ncol(df1)
 # tibbles are often used to overcome issues with design decisions of data frames
 # "lazy and surly" - tibbles do less and complain more
 library(tibble)
+
+df2 <- tibble(x = 1:3, y = letters[1:3])
+typeof(df2)
+attributes(df2)
+
+# create data frame by supplying name-vector pairs to data.frame()
+df <- data.frame(
+  x = 1:3, 
+  y = c("a", "b", "c")
+)
+str(df)
+
+# strings are converted to factors by default
+# use stringsAsFactors = false to suppress this default behavior
+df1 <- data.frame(
+  x = 1:3,
+  y = c("a", "b", "c"),
+  stringsAsFactors = FALSE
+)
+str(df1)
+
+# create tibbles with name-vector pairs like data frames
+# difference: tibbles do not coerce input
+df2 <- tibble(
+  x = 1:3, 
+  y = c("a", "b", "c")
+)
+str(df2)
