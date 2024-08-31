ebc5bf2128324efddb8c00f58b7da0722def41bf
andrewWong21 <awong541@gmail.com>
2024-08-30 23:56:35 -0400

    4.1 Introduction

---
+++ adv-r/4-subsetting_4-2.R
@@ -0,0 +1,25 @@
+# 1. What is the difference between subsetting a vector with positive integers,
+# negative integers, a logical vector, or a character vector?
+
+# 2. What’s the difference between [, [[, and $ when applied to a list?
+
+# 3. When should you use drop = FALSE?
+
+# 4. If x is a matrix, what does x[] <- 0 do? How is it different from x <- 0?
+
+# 5. How can you use a named vector to relabel categorical variables?
+
+# multiple interrelated concepts required to internalize
+# in order to master subsetting in R
+
+# six ways of subsetting atomic vectors
+
+# subsetting operators [], [[]], and $
+
+# different interactions of subsetting operators with different vector types
+# e.g. atomic vectors, lists, factors, matrices, and data frames
+
+# combining subsetting with assignment - subassignment
+
+# str() shows all pieces and structure of an object,
+# while subsetting extracts parts important to current projects
