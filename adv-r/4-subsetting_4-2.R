ec92f0f907e34a58231c424a9ef8af6e45334903
andrewWong21 <awong541@gmail.com>
2024-08-31 23:24:28 -0400

    4.2.1 Atomic vectors

--- adv-r/4-subsetting_4-2.R
+++ adv-r/4-subsetting_4-2.R
@@ -23,3 +23,64 @@
 
 # str() shows all pieces and structure of an object,
 # while subsetting extracts parts important to current projects
+
+# [] is used to select any number of elements from a vector
+# can be generalized beyond 1D vectors to objects with more complex structures
+x <- c(2.1, 4.2, 3.3, 5.4)
+
+# subsetting with positive integers returns elements at specified positions
+x[c(3, 1)]
+
+# duplicating an integer also duplicates the value
+x[c(1, 1)]
+
+# real numbers will be truncated silently - only integer part is considered
+x[c(2.1, 2.9)]
+
+# subsetting with negative integers excludes elements at specified positions
+x[-c(3, 1)]
+
+# negative integers may only be mixed with 0 when subsetting
+x[c(-1, 2)] # throws an error
+x[c(-1, 0)]
+
+# logical vectors select elements where corresponding indices are TRUE
+x[c(TRUE, TRUE, FALSE, FALSE)]
+
+# logical expressions can be used in place of raw TRUE or FALSE values
+x[x > 3]
+
+
+# recycling rules are applied to x[y] when x and y are different lengths
+# shorter vector is recycled to length of longer vector
+x[c(TRUE, FALSE)]
+x[c(TRUE, FALSE, TRUE, FALSE)]
+
+# convenient if either vector is of length 1
+# but rules are not applied consistently for other lengths in base R
+
+# missing values in index will result in missing value in output
+x[c(TRUE, TRUE, NA, FALSE)]
+
+# subsetting with [] without arguments returns original vector
+# more useful for matrices, arrays, and data frames than for vectors
+x[]
+
+# subsetting with [0] returns an empty vector, can be useful for test data
+x[0]
+
+# named vectors allow for use of character vectors when subsetting
+y <- setNames(x, letters[1:4])
+y
+y[c("d", "c", "a")]
+
+# character vector indices can be repeated
+y[c("a", "a", "a")]
+
+# names must match exactly when subsetting with character vectors
+# otherwise NA is returned for nonexistent name
+z <- c(abc = 1, def = 2)
+z[c("a", "d")]
+
+# avoid subsetting with factors - underlying integer value will be used instead
+y[factor("b")]
