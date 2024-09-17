25c6a5b67a1c566de6d81eace8b10bd0eda9bc67
andrewWong21 <awong541@gmail.com>
2024-09-16 23:59:11 -0400

    5.3.3 Exercises

--- adv-r/5-control-flow_5-3.R
+++ adv-r/5-control-flow_5-3.R
@@ -59,3 +59,60 @@ for (x in xs){
 for (i in seq_along(xs)){
   print(xs[[i]])
 }
+
+# if loops work well when start and end values to iterate over are known
+# use while() to perform an action while a given condition is true
+# use repeat() to perform an action forever, or until break keyword reached
+
+# loop flexibility (increasing): for, while, repeat
+# best to use least flexible type of loop applicable for each situation
+
+# data analysis tends to use map() and apply() for iterations
+
+# R has no equivalent syntax for performing a do-while loop
+
+
+# if() is used with scalars, ifelse() is used with vectors
+
+y <- if (x) 3
+# if x is TRUE, value of y is 3
+# if x is FALSE, value of y is NULL
+# if x is NA, statement returns error
+
+switch("x", 
+  x = ,
+  y = 2, 
+  z = 3
+)
+# result is 2 as x falls through to case y, which returns 2
+
+# -------------------------------------------------------------------------
+
+# 1. Why does this code succeed without warnings?
+x <- numeric()
+out <- vector("list", length(x))
+for (i in 1:length(x)) {
+  out[i] <- x[i] ^ 2
+}
+out
+# 1:length(x) results in 1:0
+
+
+# 2. When the following code is evaluated, what can you say about
+# the vector being iterated?
+xs <- c(1, 2, 3)
+for (x in xs) {
+  xs <- c(xs, x * 2)
+}
+xs
+# x is evaluated once at the beginning of each loop
+
+
+# 3. What does the following code tell you about when the index is updated?
+for (i in 1:3){
+  i <- i * 2
+  print(i)
+}
+
+# no error is returned, loop proceeds through 3 iterations
+# indexing variable is reassigned at beginning of each iteration
