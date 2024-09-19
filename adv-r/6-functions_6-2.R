5875d1c5978e7ea38eb49d1670b352543a59c344
andrewWong21 <awong541@gmail.com>
2024-09-18 23:57:51 -0400

    6.2.2 Primitive functions

--- adv-r/6-functions_6-2.R
+++ adv-r/6-functions_6-2.R
@@ -10,11 +10,45 @@
 # body(), code inside function
 # environment(), data structure for finding values associated with names
 
-fo2 <- function(x, y){
+f02 <- function(x, y){
   # A comment
   x + y
 }
 
-formals(fo2)
-body(fo2)
-environment(fo2)
+formals(f02)
+body(f02)
+environment(f02)
+
+# formals and body are defined explicitly when creating a function
+# environment is specified implicitly, according to where function is defined
+
+environment(mean)
+environment(dplyr::arrange)
+environment(f02)
+
+# functions may also possess attributes similarly to objects
+# srcref, short for source reference, is an attribute that points to
+# function's source code, including comments and formatting (unlike body())
+attr(f02, "srcref")
+
+# primitive functions in R directly call C code
+sum
+`[`
+`[[`
+
+# types of primitive functions may be builtin or special
+typeof(sum)
+typeof(`[`)
+typeof(`[[`)
+
+# these functions do not have a formals(), body(), or environment()
+# and attempting to call them will return NULL
+formals(sum)
+body(sum)
+environment(sum)
+
+# R functions are objects in and of themselves - "first-class functions"
+# no special syntax for creating functions
+
+# binding function object to a name is not required - functions may be anonymous
+lapply(mtcars, function(x) length(unique(x)))
