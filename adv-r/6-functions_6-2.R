e710ac437b4e063ada76bd02862b204597f8369c
andrewWong21 <awong541@gmail.com>
2024-09-20 23:58:22 -0400

    6.2.5 Exercises

--- adv-r/6-functions_6-2.R
+++ adv-r/6-functions_6-2.R
@@ -72,3 +72,83 @@ args <- list(1:10, na.rm = TRUE)
 do.call(mean, args)
 
 # -------------------------------------------------------------------------
+
+# 1. Given a name, match.fun() lets you find a function. Given a function,
+# can you find its name? Why doesn't that make sense in R?
+
+# functions may have more than one name bound to them
+# or they may be anonymous functions with no name at all
+
+
+# 2. It's possible (although typically not useful) to call an anonymous
+# function. Which of the two approaches below is correct? Why?
+
+# incorrect - calling f throws an error as it attempts to return 3()
+f <- function(x) 3()
+f()
+# correct - function is wrapped in parentheses
+(function(x) 3)()
+
+
+# 3. A good rule of thumb is that an anonymous function should fit on one line
+# and shouldn't need to use {}.
+
+# 4. What function allows you to tell if an object is a function?
+# What function allows you to tell if an object is a primitive function?
+
+# use is.function(x) to test if x is a function
+is.function(3)
+is.function(mean)
+is.function(sum)
+is.function(`[`)
+
+# use is.primitive(x) to test if x is a primitive function
+is.primitive(3)
+is.primitive(mean)
+is.primitive(sum)
+is.primitive(`[`)
+
+
+# 5. This code makes a list of all functions in the base package.
+objs <- mget(ls("package:base", all = TRUE), inherits = TRUE)
+funcs <- Filter(is.function, objs)
+objs
+funcs
+
+# Which function has the most arguments?
+t <- tibble::tibble(
+  funcs = funcs, 
+  args = sapply(lapply(funcs, formals), length)
+)
+# function with 22 arguments
+t |> 
+  dplyr::filter(args == max(args))
+
+fun <-  t |> 
+  dplyr::filter(args == max(args)) |> 
+  dplyr::select(funcs) |> 
+  dplyr::pull()
+fun
+  
+head(t)
+
+# How many base functions have no arguments?
+
+# How could you adapt the code to find all primitive functions?
+# modify funcs to Filter(is.primitive, objs)
+
+
+# 6. What are the three important components of a function?
+
+# the three components of a function are its arguments, body, and environment
+
+
+# 7. When does printing a function not show the environment it was created in?
+
+print(mean)
+# if function is primitive function or created in global environment,
+# then its environment is not printed
+print(sum)
+print(`[`)
+print(funs$double)
+print(funs$half)
