f2bf07a66625835f74d159d8aefcc85ab8863287
andrewWong21 <awong541@gmail.com>
2024-09-22 23:58:04 -0400

    6.4 Lexical scoping

--- adv-r/6-functions_6-4.R
+++ adv-r/6-functions_6-4.R
@@ -24,3 +24,25 @@ x |>
   square() |> 
   mean() |> 
   sqrt()
+
+# x |> f() is equivalent to f(x)
+# x |> f(y) is equivalent to f(x, y)
+
+# nesting is concise, but read inside out and right to left
+# arguments end up spread out over long distances
+
+# intermediate objects requires naming objects, useful if objects are important
+# intermediate values are not very useful outside of process
+
+# piping is read left to right, more straightforward than nesting
+# but can only be used for linear transformations, requires third-party package
+
+# code often combines different styles, piping is more common in data analysis
+
+# scoping is the act of finding a value associated with a name
+
+# R uses lexical scoping
+# values are looked up based on how a function is defined, not called
+
+# four primary rules of scoping
+# name masking, functions vs. variables, fresh start, dynamic lookup
